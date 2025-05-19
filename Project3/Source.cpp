1967c912f6fe5278a61a7ae551016779b07669ac
gameschool-student1 <stud1.gschl24@outlook.com>
2025-05-19 19:10:54 +0700

    j

---
+++ Project3/Source.cpp
@@ -0,0 +1,47 @@
+#include "NOVELBETA.h"
+
+
+struct location_ 
+{
+
+	string name; 
+	string descr; 
+
+};
+
+location_ location[4]; 
+
+
+void InitGame() {
+	location[0].name = "Зона удара"; 
+	location[0].descr = "Место болезненного столкновения с поверхностью планеты. Спасательная капусла ещё дымится";
+	location[1].name = "Коридор";
+	location[1].descr = "Длинный запустелый коридор";
+
+
+
+
+
+
+
+}
+
+int main() {
+	/*int temp[5] = { 1,6,-5,4,-9 };
+	temp[0] = 7;*/
+	InitGame();
+
+	for (int i = 0; i < 2; i++) {
+
+		cout << location[i].name;
+
+	
+	}
+		
+		
+	
+	
+
+
+
+}
