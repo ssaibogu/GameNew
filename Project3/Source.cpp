39fc8952dfb6552883b94156fcfc796993bff352
student4 <stud4@STUDENT4>
2025-05-30 16:46:51 +0700

    dfgdf

--- Project3/Source.cpp
+++ Project3/Source.cpp
@@ -28,10 +28,25 @@ void InitGame() {
 
 int main() {
 	setlocale(LC_ALL, "Russian");
-	/*int temp[5] = { 1,6,-5,4,-9 };
-	temp[0] = 7;*/
+	int temp[5] = { 1,6,-5,4,-9 };
+	
+	for (int i = 1; i < 5; i++) {
+		for (int j = 0; j < 5 - 1; j++) {
+			if (temp[j] > temp[j+1])
+			{
+				int temp1 = temp[j];
+				temp[j] = temp[j+1];
+				temp[j + 1] = temp1;
+			}
+		}
+	}
+	cout << " ";
+	for (int i = 0; i < 5; ++i)
+	{
+		cout << temp[i] << " ";
+	}
+	cout << endl;
 
-	// ДЗ: вывести числа из массива temp в порядке возрастания (пузырьковый метод)fghfgh
 	// Расписать локации, продумать механики
 	// Контейнеры - вектор, операции с векторами
 	InitGame();
